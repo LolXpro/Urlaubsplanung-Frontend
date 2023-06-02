7d41f384d6808412833ffd15c348cb0020ec9eac
Nico <ngross@mt-ag.com>
2023-06-02 14:22:30 +0200

    Urlaub Einreichen Seite erstellt

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  typeVal: string | undefined;
+  fromDate: Date | undefined;
+  toDate: Date | undefined;
+  description: string | undefined;
+
   constructor() {}
 
+  public submit(){
+    console.log(this.typeVal, this.fromDate, this.toDate, this.description)
+  }
+
+  public cancel() {
+    this.typeVal = ''
+    this.fromDate = undefined
+    this.toDate = undefined
+    this.description = undefined;
+  }
 }
